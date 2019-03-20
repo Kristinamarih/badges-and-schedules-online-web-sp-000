c8f911b4251b210e8579d4147cbebf265c8f00bf
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-20 05:10:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,6 @@ end
 def batch_badge_creator(attendees)
   badges = [ ]
   attendees.each do |name|
-    puts "Hello, my name is #{name}."
     badges << "Hello, my name is #{name}."
   end
   badges
